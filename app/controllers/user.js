6b7bbda620f65cc7acd8de71a8f78fa16217e61b
kenny-tech <kennyemma2008@gmail.com>
2020-05-21 17:13:58 +0100

    Created update profile endpoint

---
+++ app/controllers/user.js
@@ -0,0 +1,21 @@
+const User = require('../models/user')
+
+exports.updateProfile = (req, res) => {
+
+    User.findByIdAndUpdate(req.params.userId, req.body, function(err, user) {
+        if (err) {
+            res.status(422).send({ 
+                success: false,
+                data: null,
+                message: err + ': Unable to update profile. Please try again',
+            });
+        } else {
+            res.status(200).send({ 
+                success: true,
+                data: req.body,
+                message: 'Profile successfully updated',
+            });
+        }
+    })
+}
+
